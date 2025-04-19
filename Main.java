c6f93c49c45a0eca69107b54fece6abbe041d2d0
John McCloskey <JMcClos3@outlook.com>
2025-04-19 12:25:00 -0400

    Worked on main

--- Main.java
+++ Main.java
@@ -1,2 +1,14 @@
+
+import java.util.*;
+import java.io.*;
+import java.time.LocalDate;
+
 public class Main {
+    private List<Income> incomes = new ArrayList<>();
+    private List<Expense> expenses = new ArrayList<>();
+
+    public void addIncome (Income income) {
+        incomes.add(income);
+    }
+
 }
