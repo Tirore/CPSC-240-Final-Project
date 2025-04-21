9db011d77dfd8eabc36523e7fcc0aadf278d693a
John McCloskey <JMcClos3@outlook.com>
2025-04-21 14:35:12 -0400

    Worked on main.

--- Income.java
+++ Income.java
@@ -1,2 +1,25 @@
+
 public class Income {
+    private String type;
+    private double amount;
+
+    public Income (String type, double amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Income from " + type + ": $" + amount;
+    }
+
+
 }
