3bb5cfba00d0ca94d8bf073b9bc18c89a585ac84
John McCloskey <JMcClos3@outlook.com>
2025-04-22 15:12:51 -0400

    Merge remote-tracking branch 'origin/main'

--- GUI.java
+++ GUI.java
@@ -4,5 +4,8 @@ import java.awt.event.*;
 import java.io.IOException;
 
 public class GUI {
+    private Main manager = new Main();
+    private JLabel incomeLabel, expenseLabel, balanceLabel;
+    private JTextArea remindersArea;
 
 }
