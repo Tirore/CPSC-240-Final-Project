a1b64a63e88e8d0951d18952b4aca911be1f3907
John McCloskey <JMcClos3@outlook.com>
2025-04-22 15:46:41 -0400

    Merge remote-tracking branch 'origin/main'

--- Main.java
+++ Main.java
@@ -9,31 +9,31 @@ import java.util.*;
 import java.io.*;
 import java.time.LocalDate;
 
-public class Main {
+public class Main { //Creates lists to dtores income and expense inputs.
     private List<Income> incomes = new ArrayList<>();
     private List<Expense> expenses = new ArrayList<>();
 
     public void addIncome (Income income) {
         incomes.add(income);
-    }
+    } //Adds income to list.
 
     public void addExpense (Expense expense) {
         expenses.add(expense);
-    }
+    } //Adds new expense to list.
 
-    public double getTotalIncome () {
+    public double getTotalIncome () {//Adds up all income entries to calculate total
         return incomes.stream().mapToDouble(Income::getAmount).sum();
+    } .
+
+    public double getTotalExpenses () { //Finds sum of all expenses.
+        return expenses.stream().mapToDouble(Expense::getAmount).sum();
     }
 
-    public double getTotalExpenses () {
-        return expenses.stream().mapToDouble(Income::getAmount).sum();
-    }
-
-    public double getBalance () {
+    public double getBalance () { //Finds balance by subtracting expenses from incomes.
         return getTotalIncome() - getTotalExpenses();
     }
 
-    public List<Expense> getDueExpenses() {
+    public List<Expense> getDueExpenses() {//Lists expenses due in next week.
         List<Expense> due = new ArrayList<>();
         for (Expense e : expenses) {
             if (e.isDue()) due.add(e);
@@ -41,7 +41,7 @@ public class Main {
         return due;
     }
 
-    public void genReport(String filePath) throws IOException {
+    public void genReport(String filePath) throws IOException {//Writes incomes, expenses, and balance to file.
         BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
         writer.write("=== Budget Report ===\n");
         writer.write("Incomes:\n");
@@ -54,7 +54,7 @@ public class Main {
         writer.close();
     }
 
-    public void importData( String filePath) throws IOException {
+    public void importData( String filePath) throws IOException {//Imports data from .txt file
         BufferedReader reader = new BufferedReader(new FileReader(filePath));
         String line;
         while ((line = reader.readLine()) != null) {
