c6c6c67d46c2ae774d5f0323867463dd00d0cbf8
John McCloskey <JMcClos3@outlook.com>
2025-04-22 14:30:00 -0400

    Merge remote-tracking branch 'origin/main'

--- Expense.java
+++ Expense.java
@@ -2,4 +2,35 @@ import java.time.LocalDate;
 
 
 public class Expense {
+    private String type;
+    private double amount;
+    private LocalDate due;
+
+    public Expense(String type, double amount, LocalDate due) {
+        this.type = type;
+        this.amount = amount;
+        this.due = due;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDate getDue() {
+        return due;
+    }
+
+    public boolean isDue() {
+        return LocalDate.now().plusDays(7).isAfter(due);
+    }
+
+    @Override
+    public String toString() {
+        return "Expense for " + type + ": $" + amount + " due " + due;
+    }
+
 }
